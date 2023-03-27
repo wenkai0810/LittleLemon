4296976b96bec10d2a299d8fbc52adf2d44d8441
wenkai0810 <wenkaiong123@gmail.com>
2023-03-27 11:28:19 +0800

    sb

--- Assignment/LittleLemon/restaurant/models.py
+++ Assignment/LittleLemon/restaurant/models.py
@@ -17,5 +17,7 @@ class Menu(models.Model):
    price = models.DecimalField(max_digits=10,decimal_places=2) 
    Inventory = models.IntegerField()
    def get_item(self):
-      return f'{self.title} : {str(self.price)}'
+    return f'{self.title} : {str(self.price)}'
+   def __str__(self):
+    return f'{self.title} : {str(self.price)}'
 
--- Assignment/LittleLemon/restaurant/views.py
+++ Assignment/LittleLemon/restaurant/views.py
@@ -11,12 +11,10 @@ def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
 class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
